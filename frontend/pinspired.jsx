be4b87c2015da8f9aa92c9d2626b3a7d092a0042
Amanda Chen <amandachen@Amandas-MacBook-Pro.local>
2017-06-20 11:25:51 -0400

    Add actions to window for testing

--- frontend/pinspired.jsx
+++ frontend/pinspired.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// TESTING
+import { signup, login, logout } from './util/session_api_util';
+window.signup = signup;
+window.login = login;
+window.logout = logout;
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Pinspired</h1>, root);
