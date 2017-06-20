d77db0e7b1551b839f09fb356ce063cb032a292b
Amanda Chen <amandachen@Amandas-MacBook-Pro.local>
2017-06-20 11:37:29 -0400

    Add store methods to window for testing

--- frontend/pinspired.jsx
+++ frontend/pinspired.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 // TESTING
 import { signup, login, logout } from './util/session_api_util';
 window.signup = signup;
 window.login = login;
 window.logout = logout;
+//
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  // TESTING
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  //
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Pinspired</h1>, root);
 });
