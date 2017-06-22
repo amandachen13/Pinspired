cb8af2d3201c626cf9c680a333f65a350e381cad
Amanda Chen <amandachen@Amandas-MacBook-Pro.local>
2017-06-22 18:31:57 -0400

    Create pin actions and ajax requests

---
+++ frontend/actions/pin_actions.js
@@ -0,0 +1,4 @@
+import * as APIUtil from './../util/pin_api_util';
+
+export const RECEIVE_ALL_PINS = "RECEIVE_ALL_PINS";
+export const RECEIVE_SINGLE_PIN = "RECEIVE_SINGLE_PIN";
---
+++ frontend/util/pin_api_util.js
@@ -0,0 +1,36 @@
+export const fetchAllPins = () => (
+  $.ajax({
+    method: 'GET',
+    url: '/api/pins'
+  })
+);
+
+export const fetchSinglePin = id => (
+  $.ajax({
+    method: 'GET',
+    url: `api/pins/${id}`
+  })
+);
+
+export const createPin = pin => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/pins',
+    data: pin
+  })
+);
+
+export const updatePin = pin => (
+  $.ajax({
+    method: 'PATCH',
+    url: `/api/pins/${pin.id}`,
+    data: {pin}
+  })
+);
+
+export const deletePin = id => (
+  $.ajax({
+    method: 'DELETE',
+    url: `api/pins/${id}`
+  })
+);
