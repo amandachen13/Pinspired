b516d0e739fdda9a2abeb03da022d9d2e7a3f12a
Amanda Chen <amandachen13@gmail.com>
2017-07-19 15:39:28 -0400

    Style boards list on user profile

--- frontend/components/users/user_pins_index.jsx
+++ frontend/components/users/user_pins_index.jsx
@@ -68,43 +68,45 @@ class UserPinsIndex extends React.Component {
   // }
 
   pinsList() {
-    //debugger
     const user = this.props.users[this.props.username];
-    const pins = this.pinsIndexArr().map( id => {
+    const pins = [];
+    this.pinsIndexArr().forEach( id => {
       //debugger
       const pin = user.pins[id];
-      //debugger
-      return (
-        <li key={pin.id}>
-          <div className="pins-hover">
-            <div className="pins">
-              <div onClick={ () => this.props.history.push(`/pin/${pin.id}`) } >
-                <div className="pins-image">
-                  <img className="pins" src={pin.image_url} />
-                  <div className="dim-gradient">
-                    <a className="pin-url" href={`${pin.url}`} target="_blank">{pin.url}</a>
+      if (user.boards[pin.board_id]) {
+        pins.push (
+          <li key={pin.id}>
+            <div className="pins-hover">
+              <div className="pins">
+                <div onClick={ () => this.props.history.push(`/pin/${pin.id}`) } >
+                  <div className="pins-image">
+                    <img className="pins" src={pin.image_url} />
+                    <div className="dim-gradient">
+                      <a className="pin-url" href={`${pin.url}`} target="_blank">{pin.url}</a>
+                    </div>
+                    <div onClick={ e => { e.stopPropagation(); this.props.open(<PinSaveFormContainer pin={this.props.pins[pin.id]}/>);} } className="pin-save-modal">
+                      <i className="fa fa-thumb-tack" aria-hidden="true"></i>
+                      <div className="pin-save">Save</div>
+                    </div>
                   </div>
-                  <div onClick={ e => { e.stopPropagation(); this.props.open(<PinSaveFormContainer pin={this.props.pins[pin.id]}/>);} } className="pin-save-modal">
-                    <i className="fa fa-thumb-tack" aria-hidden="true"></i>
-                    <div className="pin-save">Save</div>
+                  <div className="pin-info">
+                    <div className="pin-title">{pin.title}</div>
+                    <div className="pin-desc">{pin.description}</div>
                   </div>
                 </div>
-                <div className="pin-info">
-                  <div className="pin-title">{pin.title}</div>
-                  <div className="pin-desc">{pin.description}</div>
-                </div>
-              </div>
-              <div onClick={ () => this.props.history.push(`/${user.username}/board/${pin.board_id}`) } className="pin-link-to-board">
-                <img className="pin-creator-pic" src={user.image_url} />
-                <div className="pin-creator-info">
-                  <span className="pin-creator">{user.username}</span>
-                  <span className="pin-board">{user.boards[pin.board_id].name}</span>
+                <div onClick={ () => this.props.history.push(`/${user.username}/board/${pin.board_id}`) } className="pin-link-to-board">
+                  <img className="pin-creator-pic" src={user.image_url} />
+                  <div className="pin-creator-info">
+                    <span className="pin-creator">{user.username}</span>
+                    <span className="pin-board">{user.boards[pin.board_id].name}</span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </li>
-      );
+          </li>
+        );
+      }
+      //debugger
     });
     //debugger
     if (user.username === this.props.currentUser.username) {
@@ -128,7 +130,7 @@ class UserPinsIndex extends React.Component {
   // }
 
   render() {
-
+    debugger
     // return (
     //   <div>{this.props.board.name}</div>
     // );
