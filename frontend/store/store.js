8758315461cc1fe3e3e96aefa39f4ba3d2ea5e58
Amanda Chen <amandachen13@gmail.com>
2017-08-17 18:47:06 -0400

    Try adding logger again

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/root_reducer';
 
@@ -7,15 +7,16 @@ const middlewares = [thunk];
 
 // if (process.env.NODE_ENV !== 'production') {
   // must use 'require' (import only allowed at top of file)
-  const {createLogger} = require('redux-logger');
-  middlewares.push(createLogger());
+  // const {createLogger} = require('redux-logger');
+  // middlewares.push(createLogger());
 // }
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(...middlewares)
+    // applyMiddleware(...middlewares)
+    applyMiddleware(thunk, logger)
   )
 );
 
